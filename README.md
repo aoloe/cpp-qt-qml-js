922f7b7dfac546e13a447926cfa678e27902fec9
ale rimoldi <ale@graphicslab.org>
2017-07-12 19:18:07 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# Qt Sample Application using QML and js as script engine
+
+
+
---
+++ main.cpp
@@ -0,0 +1,15 @@
+#include <QDebug>
+// #include <QApplication>
+
+#include "sample.h"
+#include "mainwindow.h"
+
+int main(int argc, char *argv[])
+{
+    Sample app(argc, argv);
+
+    MainWindow mainWin;
+    mainWin.show();
+
+    return app.exec();
+}
